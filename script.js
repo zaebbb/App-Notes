945026ef7ee16860eca5324462829cf2225ed9bd
Vladimir Zaeb <65035479+zaebbb@users.noreply.github.com>
2020-12-23 19:39:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const addBtn = document.getElementById("add");
+const copyright = document.querySelector(".copyright")
+
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes){
+    notes.forEach(note => {
+        addNewNote(note)
+    })
+}
+
+addBtn.addEventListener("click", () => {
+    addNewNote();
+    copyright.style.opacity = 0;
+});
+
+function addNewNote(text = ""){
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    note.innerHTML = `
+    <div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash"></i></button>
+        </div>
+        <div class="main hidden"></div>
+        <textarea placeholder="Напишите заметку!" rows="10"></textarea>
+	</div>
+    `;
+
+    const editBtn = note.querySelector(".edit");
+    const deleteBtn = note.querySelector(".delete");
+
+    const main = note.querySelector(".main");
+    const textArea = note.querySelector   ("textarea");
+
+    textArea.value = text;
+
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+        editBtn.classList.toggle("activeColor");
+    });
+    textArea.addEventListener("input", (e) => {
+        const { value } = e.target;
+
+        main.innerHTML = marked(value);
+
+        updateLS();
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        note.remove();
+    });
+
+    document.body.appendChild(note)
+}
+
+function updateLS(){
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
